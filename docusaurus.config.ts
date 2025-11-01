7cc8473852ea1176cfab0160b70044c51d16969e
Pritam tirpude <pritam.tirpude9601@gmail.com>
2025-11-01 20:29:13 +0530

    remove edit page configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -41,25 +41,6 @@ const config: Config = {
       {
         docs: {
           sidebarPath: "./sidebars.ts",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/pritamtirpude/layout-designer-docs/tree/master/docs",
-        },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ["rss", "atom"],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/pritamtirpude/layout-designer-docs/tree/master/docs",
-          // Useful options to enforce blogging best practices
-          onInlineTags: "warn",
-          onInlineAuthors: "warn",
-          onUntruncatedBlogPosts: "warn",
         },
         theme: {
           customCss: "./src/css/custom.css",
